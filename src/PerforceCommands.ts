c6b4adee4e861f19e8b0d7d4cd659f9ff0679137
mjcrouch <49367953+mjcrouch@users.noreply.github.com>
2020-03-16 19:31:59 +0000

    add edit and save command

--- src/PerforceCommands.ts
+++ src/PerforceCommands.ts
@@ -1,6 +1,6 @@
 "use strict";
 
-import { commands, workspace, window, Uri, QuickPickItem } from "vscode";
+import { commands, workspace, window, Uri, QuickPickItem, Disposable } from "vscode";
 
 import * as Path from "path";
 
@@ -29,6 +29,15 @@ export namespace PerforceCommands {
         commands.registerCommand("perforce.login", login);
         commands.registerCommand("perforce.diffFiles", diffFiles);
         commands.registerCommand("perforce.menuFunctions", menuFunctions);
+    }
+
+    export function registerImportantCommands(subscriptions: Disposable[]) {
+        subscriptions.push(
+            commands.registerCommand(
+                "perforce.editAndSave",
+                editAndSaveOpenFileOrPassthrough
+            )
+        );
     }
 
     function addOpenFile() {
@@ -70,6 +79,23 @@ export namespace PerforceCommands {
         }
 
         p4edit(editor.document.uri);
+    }
+
+    async function editAndSaveOpenFileOrPassthrough() {
+        const activeFile = window.activeTextEditor?.document;
+        if (!activeFile) {
+            // pass through to the save action in case it can do anything else
+            await commands.executeCommand("workbench.action.files.save");
+        } else {
+            try {
+                await p4edit(activeFile.uri);
+            } catch (err) {
+                // ensure save always happens even if something goes wrong
+                Display.showError(err);
+            }
+
+            await activeFile.save();
+        }
     }
 
     export function p4edit(fileUri: Uri): Promise<boolean> {
