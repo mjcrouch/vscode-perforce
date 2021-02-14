c1b25a660d19b66220cd5a22eeabb4c739a8acef
mjcrouch <49367953+mjcrouch@users.noreply.github.com>
2021-02-14 10:13:43 +0000

    propagate decorations to parent folders

--- src/scm/DecorationProvider.ts
+++ src/scm/DecorationProvider.ts
@@ -60,6 +60,7 @@ export class DecorationProvider {
             color: colorName
                 ? new ThemeColor("perforceDecoration." + colorName + "Foreground")
                 : undefined,
+            propagate: true,
         };
     }
 
